1d942809d507a9a36a2e1aaafcbce179d9c03baf
Ivan Lara <ukikalix@gmail.com>
2023-05-15 23:35:48 -0400

    fX: add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import svelte from "@astrojs/svelte";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), react(), svelte()],
-    output: "server",
-    suppressHydrationWarning: true,
+  integrations: [tailwind(), react(), svelte()],
+  output: "server",
+  suppressHydrationWarning: true,
+  adapter: vercel()
 });
